1c783c61790d7501c156da453a34cd76d92e8f1c
Brian Hayes <bjoeyhayes@gmail.com>
2018-03-22 14:52:20 -0400

    move the ship display from onHover to the game board

--- src/containers/GameBoard.js
+++ src/containers/GameBoard.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+import {generateLocations, coordinatesMatch} from '../engine/engine-helpers';
+import locationsToCssGrid from '../util/locationsToCssGrid';
+
 // import Grid from '../components/Grid';
 import SquareCellGrid from '../components/SquareCellGrid';
 import Marker from '../components/Marker';
@@ -9,7 +12,9 @@ import Ship from '../components/Ship';
 export default class GameBoard extends Component {
   static propTypes = {
     activePlayer: PropTypes.object.isRequired,
-    inactivePlayer: PropTypes.object.isRequired
+    inactivePlayer: PropTypes.object.isRequired,
+    selectedShip: PropTypes.object.isRequired,
+    hoveredCoordinates: PropTypes.array
   }
   renderMarkers(player, type) {
     let markers = [];
@@ -24,6 +29,32 @@ export default class GameBoard extends Component {
     }
     return markers;
   }
+  renderShipOnHover() {
+    const {name, size, orientation} = this.props.selectedShip;
+    const coordinates = this.props.hoveredCoordinates;
+    if (this.props.gameStage === 'setup') {
+      if (
+        coordinates !== null &&
+        name !== null &&
+        size !== null &&
+        orientation !== null
+      ) {
+        const locations = generateLocations(
+          coordinates,
+          size,
+          orientation
+        );
+        const style = {
+          gridArea: locationsToCssGrid(locations),
+          background: '#ccc',
+          transition: 'all 50ms ease-in'
+        };
+        return <div className="hover-ship" style={style}></div>;
+      }
+    }
+    return null;
+
+  }
   render() {
 
     let ships = [];
@@ -32,14 +63,14 @@ export default class GameBoard extends Component {
         ships.push(
           <Ship
             name={this.props.activePlayer.ships[i].name}
-            damage={this.props.activePlayer.ships[i].damage}
+            // damage={this.props.activePlayer.ships[i].damage}
             locations={this.props.activePlayer.ships[i].locations}
             key={this.props.activePlayer.ships[i].name}
           />
         );
       }
     }
-
+    const hoverDisplay = this.renderShipOnHover();
     return (
       <div className="gameboard" ref="gameboard">
         <div className="enemy-grid">
@@ -50,6 +81,7 @@ export default class GameBoard extends Component {
         </div>
         <div className="player-grid">
           <SquareCellGrid dimensions={[10, 10]}>
+            {hoverDisplay}
             {ships}
             {this.renderMarkers(this.props.activePlayer, 'hits')}
             {this.renderMarkers(this.props.activePlayer, 'misses')}
